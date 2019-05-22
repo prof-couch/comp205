7129277f76318d2ac4b67b5dd4c8812291af8459
prof-couch <50831459+prof-couch@users.noreply.github.com>
2019-05-22 16:36:56 -0400

    Conditional statement exercise

---
+++ exercises/Conditional statements/tests/q2.py
@@ -0,0 +1,22 @@
+test = {
+  'name': 'Question 2',
+  'points': 1,
+  'suites': [
+    {
+      'cases': [
+        {
+          'code': r"""
+          >>> states
+          ['MA', 'CA', 'MA']
+          """,
+          'hidden': False,
+          'locked': False
+        }
+      ],
+      'scored': True,
+      'setup': '',
+      'teardown': '',
+      'type': 'doctest'
+    }
+  ]
+}
---
+++ exercises/Conditional statements/tests/q3.py
@@ -0,0 +1,22 @@
+test = {
+  'name': 'Question 3',
+  'points': 1,
+  'suites': [
+    {
+      'cases': [
+        {
+          'code': r"""
+          >>> cleaned
+          [('lawnmower', 100.0, 7.0, 'MA')]
+          """,
+          'hidden': False,
+          'locked': False
+        }
+      ],
+      'scored': True,
+      'setup': '',
+      'teardown': '',
+      'type': 'doctest'
+    }
+  ]
+}
