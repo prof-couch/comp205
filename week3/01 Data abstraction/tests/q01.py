f6a8582884d944dae124a52a0f4b38a3c14df3f0
prof-couch <50831459+prof-couch@users.noreply.github.com>
2019-06-05 16:52:20 -0400

    First version of exercise 01

---
+++ week3/01 Data abstraction/tests/q01.py
@@ -0,0 +1,24 @@
+test = {
+  'name': 'Question 1',
+  'points': 1,
+  'suites': [
+    {
+      'cases': [
+        {
+          'code': r"""
+          >>> type(x)
+          <class 'numpy.ndarray'>
+          >>> (x == np.array([1,2,3,4,5])).all()
+          True
+          """,
+          'hidden': False,
+          'locked': False
+        }
+      ],
+      'scored': True,
+      'setup': '',
+      'teardown': '',
+      'type': 'doctest'
+    }
+  ]
+}
