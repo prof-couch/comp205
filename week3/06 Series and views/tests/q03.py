57448a27412adeac742b1c6fa615fd92fd61c4e1
Alva Couch <couch@hlgn.eecs.tufts.edu>
2019-06-12 16:47:58 -0400

    New exercise

---
+++ week3/06 Series and views/tests/q03.py
@@ -0,0 +1,25 @@
+test = {
+  'name': 'Question 3',
+  'points': 1,
+  'suites': [
+    {
+      'cases': [
+        {
+          'code': r"""
+          >>> print(dogs)
+                      name kind  weight    food
+          name                                 
+          Snoopy    Snoopy  dog      16  canned
+          Dogbert  Dogbert  dog      10    pate
+          """,
+          'hidden': False,
+          'locked': False
+        }
+      ],
+      'scored': True,
+      'setup': '',
+      'teardown': '',
+      'type': 'doctest'
+    }
+  ]
+}
---
+++ week3/06 Series and views/tests/q04.py
@@ -0,0 +1,25 @@
+test = {
+  'name': 'Question 4',
+  'points': 1,
+  'suites': [
+    {
+      'cases': [
+        {
+          'code': r"""
+          >>> print(dogs)
+                      name kind  weight    food
+          name                                 
+          Snoopy    Snoopy  dog      16  canned
+          Dogbert  Dogbert  dog      25    pate
+          """,
+          'hidden': False,
+          'locked': False
+        }
+      ],
+      'scored': True,
+      'setup': '',
+      'teardown': '',
+      'type': 'doctest'
+    }
+  ]
+}
