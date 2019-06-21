57955470fa0121565b7accd29c06bdcddb26e31c
prof-couch <50831459+prof-couch@users.noreply.github.com>
2019-06-21 10:51:17 -0400

    week4 exercises

---
+++ week4/01 Applying Python/tests/q03.py
@@ -0,0 +1,25 @@
+test = {
+  'name': 'Question 3',
+  'points': 1,
+  'suites': [
+    {
+      'cases': [
+        {
+          'code': r"""
+          >>> print(d3)
+             x  y  z
+          a  1  2  3
+          b  4  5  6
+          c  7  8  9
+          """,
+          'hidden': False,
+          'locked': False
+        }
+      ],
+      'scored': True,
+      'setup': '',
+      'teardown': '',
+      'type': 'doctest'
+    }
+  ]
+}
---
+++ week4/02 Classification/tests/q05.py
@@ -0,0 +1,30 @@
+test = {
+  'name': 'Question 5',
+  'points': 1,
+  'suites': [
+    {
+      'cases': [
+        {
+          'code': r"""
+          >>> test_results.shape
+          (20,)
+          """,
+          'hidden': False,
+          'locked': False
+        },
+        {
+          'code': r"""
+          >>> type(test_results)
+          <class 'numpy.ndarray'>
+          """,
+          'hidden': False,
+          'locked': False
+        }
+      ],
+      'scored': True,
+      'setup': '',
+      'teardown': '',
+      'type': 'doctest'
+    }
+  ]
+}
